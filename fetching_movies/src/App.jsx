2bc1c0055c1fb043459f6ff92b7fe7716a90bcb9
CoolShivas <shivas2710cool00@gmail.com>
2024-04-19 13:26:08 +0530

    starting of fetching movies pro

---
+++ fetching_movies/src/App.jsx
@@ -0,0 +1,15 @@
+import AppName from "./components/AppName";
+
+
+function App(){
+  return <> 
+  <header>
+  <AppName></AppName>
+  </header>
+  <main></main>
+  <footer></footer>
+  
+   </>
+}
+
+export default App;
---
+++ fetching_movies/src/components/AppName.jsx
@@ -0,0 +1,9 @@
+
+
+const AppName = () => {
+  return (
+    <h1> Fetching Movies </h1>
+  )
+}
+
+export default AppName
